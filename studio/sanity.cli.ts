4aa0c2ac8ceaa53caefcf3e8403b36fab2d955e9
David Olaniyan <davidolaniyan8@gmail.com>
2025-10-18 13:27:16 -0600

    chore(sanity): pin appId in sanity.cli.ts to skip deploy prompts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,6 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
-  api: {projectId: 'x77edsca', dataset: 'production'}, // <-- your id
-  deployment: {autoUpdates: true},
+  api: {
+    projectId: 'x77edsca',
+    dataset: 'production',
+  },
+  deployment: {
+    autoUpdates: true,
+    appId: 'fheio59hs0nxbjdlwtr0evc1',
+  },
 })
